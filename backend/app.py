30e007270189def1bfc2d5a9950ebbe2847dbe9e
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2025-02-06 20:03:58 -0500

    Integrate DB into Flask endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
-from dataclasses import asdict
-from datetime import datetime
+from pathlib import Path
+
+import db
 from flask import Flask, request
 from flask_cors import CORS
-
-from models.restaurant import MOCK_RESTAURANTS, Restaurant
+from models.restaurant import Restaurant
 
 app = Flask(__name__)
+db.init_app(app)
+
 
 # Enables CORS for all domains on all routes
 # Read more: https://github.com/corydolphin/flask-cors?tab=readme-ov-file#simple-usage
@@ -20,50 +22,29 @@ def hello():
 @app.get('/api/restaurants')
 def get_restaurants():
     try:
-        # Not guaranteed lower-case, use LOWER() in DB query
+        # SQLite FTS5 is not case-sensitive
         search_term = request.args.get('q', '').strip() or None
         results = _search_restaurants(search_term)
         return {
-            'data': [asdict(r) for r in results]
+            'data': results
         }, 200
     except Exception as e:
+        print(f"{type(e).__name__}({e})")
         return {
             'error': str(e)
         }, 500
 
 def _search_restaurants(search_term: str | None = None) -> list[Restaurant]:
-    if not search_term:
-        return MOCK_RESTAURANTS
-    
-    search = search_term.lower()
-    return [
-        r for r in MOCK_RESTAURANTS
-        if (search in r.name.lower() or 
-            search in r.address.lower() or
-            search in r.city.lower() or
-            search in r.state.lower())
-    ]
+    sql_file = Path("queries") / "list_restaurants.sql"
+    params = ()
 
-# MySQL query for future implementation
-SEARCH_QUERY = """
-    -- created_at not shown to users
-    SELECT restaurant_id, name, address, city, state, zip_code, phone
-    FROM restaurants
-    WHERE 
-        LOWER(name) LIKE CONCAT('%', LOWER(%s), '%') OR
-        LOWER(address) LIKE CONCAT('%', LOWER(%s), '%') OR
-        LOWER(city) LIKE CONCAT('%', LOWER(%s), '%') OR
-        LOWER(state) LIKE CONCAT('%', LOWER(%s), '%')
-    ORDER BY name
-"""
-
-GET_ALL_QUERY = """
-    -- created_at not shown to users
-    SELECT restaurant_id, name, address, city, state, zip_code, phone
-    FROM restaurants
-    ORDER BY name
-"""
-
+    if search_term:
+        sql_file = Path("queries") / "search_restaurants.sql"
+        params = (search_term,)
+        
+    query = sql_file.read_text(encoding="utf-8")
+    rows = db.query_db(query, args=params)
+    return [dict(row) for row in rows]
 
 if __name__ == '__main__':
     app.run(debug=True)
