8e690423a3c46e5a2b59c6a4819944d9c6dc16d6
Kevin Nguyen <kc2nguye@uwaterloo.ca>
2025-02-06 21:53:25 -0500

    Update CREATE TABLE constraints, datatypes, and notes on type enforcement

--- backend/sql/create_tables.sql
+++ backend/sql/create_tables.sql
@@ -1,55 +1,66 @@
--- Create tables required by the application
-CREATE TABLE Users (
-  uid INT NOT NULL PRIMARY KEY,
-  username TEXT UNIQUE,
+-- Create tables required by the application if they do not already exist
+
+CREATE TABLE IF NOT EXISTS Users (
+  uid INTEGER PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
   first_name TEXT NOT NULL,
-  last_name TEXT,
-  email TEXT NOT NULL UNIQUE,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE COLLATE NOCASE,
   password_hash TEXT NOT NULL,
-  created_at TEXT NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Restaurants (
-  restaurant_id INT NOT NULL PRIMARY KEY,
+-- Need to enforce decimal precision at application level, SQLite will treat DECIMAL(m,n) as REAL
+CREATE TABLE IF NOT EXISTS Restaurants (
+  restaurant_id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  address TEXT,
-  city TEXT,
-  state TEXT,
-  zip_code TEXT,
-  phone TEXT,
-  created_at TEXT NOT NULL,
-  average_rating DECIMAL(3, 2) CHECK(average_rating IS NULL OR average_rating = 0 OR average_rating BETWEEN 1 and 5)
+  address TEXT NOT NULL,
+  city TEXT NOT NULL,
+  state TEXT NOT NULL,
+  zip_code TEXT NOT NULL,
+  phone TEXT NOT NULL,
+  average_rating DECIMAL(3,2) NOT NULL CHECK((average_rating = 0) OR 
+                                             (average_rating BETWEEN 1 AND 5)),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  UNIQUE(name, address)
 );
 
-CREATE TABLE RestaurantImages (
-  image_id INT NOT NULL PRIMARY KEY,
-  restaurant_id INT NOT NULL REFERENCES Restaurants(restaurant_id),
-  image_url TEXT,
-  created_at TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS RestaurantImages (
+  image_id INTEGER PRIMARY KEY,
+  restaurant_id INTEGER NOT NULL REFERENCES Restaurants(restaurant_id) ON DELETE CASCADE,
+  image_url TEXT NOT NULL UNIQUE,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE RestaurantTypes (
-  type_id INT NOT NULL PRIMARY KEY,
-  type_name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS RestaurantTypes (
+  type_id INTEGER PRIMARY KEY,
+  type_name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE RestaurantTypeAssignments (
-  restaurant_id INT NOT NULL REFERENCES Restaurants(restaurant_id),
-  type_id INT NOT NULL REFERENCES RestaurantTypes(type_id),
+CREATE TABLE IF NOT EXISTS RestaurantTypeAssignments (
+  restaurant_id INTEGER REFERENCES Restaurants(restaurant_id) ON DELETE CASCADE,
+  type_id INTEGER REFERENCES RestaurantTypes(type_id) ON DELETE CASCADE,
+  
   PRIMARY KEY(restaurant_id, type_id)
 );
 
-CREATE TABLE Reviews (
-  uid INT NOT NULL REFERENCES Users(uid),
-  restaurant_id INT NOT NULL REFERENCES Restaurants(restaurant_id),
-  rating INT NOT NULL CHECK(rating BETWEEN 1 AND 5),
-  review_text TEXT,
-  created_at TEXT NOT NULL,
+-- Need to enforce review length at application level, SQLite will treat VARCHAR(n) as TEXT
+CREATE TABLE IF NOT EXISTS Reviews (
+  uid INTEGER REFERENCES Users(uid) ON DELETE CASCADE,
+  restaurant_id INTEGER REFERENCES Restaurants(restaurant_id) ON DELETE CASCADE,
+  rating INTEGER NOT NULL CHECK(rating BETWEEN 1 AND 5),
+  review_text VARCHAR(250),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
   PRIMARY KEY(uid, restaurant_id)
 );
 
-CREATE TABLE Followers (
-  uid INT NOT NULL REFERENCES Users(uid),
-  follower_id INT NOT NULL REFERENCES Users(uid),
-  CHECK(uid != follower_id)
+CREATE TABLE IF NOT EXISTS Followers (
+  uid INTEGER REFERENCES Users(uid) ON DELETE CASCADE,
+  follower_id INTEGER REFERENCES Users(uid) ON DELETE CASCADE,
+
+  CHECK(uid <> follower_id),
+
+  PRIMARY KEY(uid, follower_id)
 );
