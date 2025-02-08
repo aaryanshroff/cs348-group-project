3ee57efd9a4e671778f887cf1785b8cbff1f7c04
DJHoon24 <djhoon24@gmail.com>
2025-02-07 19:01:31 -0500

    Fix sql queries for insert

--- backend/sql/test-sample.sql
+++ backend/sql/test-sample.sql
@@ -1,6 +1,7 @@
+-- R6 Add restaurants and reviews (DJ)
 -- Adding a Restaurant Query
-INSERT INTO Restaurants (restaurant_id, name, address, city, state, zip_code, phone, average_rating)
-VALUES(1, 'McDonalds', '123 Rose St', 'Toronto', 'ON', '11111', '111-111-1111', 0.0);
+INSERT INTO Restaurants (name, address, city, state, zip_code, phone, avg_rating)
+VALUES('McDonalds', '123 Rose St', 'Toronto', 'ON', '11111', '111-111-1111', 0.0);
 
 INSERT INTO RestaurantImages(image_id, restaurant_id, image_url)
 VALUES(1, 1, 'image_url');
@@ -10,10 +11,10 @@ INSERT INTO Reviews (uid, restaurant_id, rating, review_text)
 VALUES (1, 1, 5, 'great store');
 
 -- Finding all reviews for a Restaurant Query to show on the page:
-SELECT * FROM Reviews WHERE restaurant_id = 1;
+SELECT uid, rating, review_text, created_at FROM Reviews WHERE restaurant_id = 1;
 
--- Finding the current user’s reviews for a restaurant:
-SELECT * FROM Reviews WHERE restaurant_id = 1 AND uid = 1;
+-- Finding the user uid=1's reviews for a restaurant:
+SELECT rating, review_text, created_at FROM Reviews WHERE restaurant_id = 1 AND uid = 1;
 
 -- R7 List all restaurants and filter restaurants by city and type (Aaryan)
 
