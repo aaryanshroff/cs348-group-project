002be1cb295a797a40d85eeabb73a055218c6b75
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2025-02-08 22:29:13 -0500

    Add React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import Restaurants from './Restaurants.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Restaurants />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
