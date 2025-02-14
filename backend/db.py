14fb414e48310e4708dde5b5c45c5ebd01db9d4d
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2025-02-13 20:23:59 -0500

    Use DB_FILEPATH

--- backend/db.py
+++ backend/db.py
@@ -3,36 +3,41 @@ import sqlite3
 
 from flask import Flask, g
 
+
 def get_db() -> sqlite3.Connection:
-    # g is a special object that is unique for each request. 
+    # g is a special object that is unique for each request.
     # It is used to store data that might be accessed by multiple functions during the request.
-    if 'db' not in g:
+    if "db" not in g:
         g.db = sqlite3.connect(
-            os.environ['DB_PATH'],
+            os.environ["DB_FILEPATH"],
         )
         # sqlite3.Row tells the connection to return rows that behave like dicts. This allows accessing the columns by name.
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
+
 def close_db(exception=None):
     """
     Close a DB connection if it exists.
     """
-    db = g.pop('db', None)
+    db = g.pop("db", None)
 
     if db is not None:
         db.close()
 
+
 def init_db() -> None:
     db = get_db()
 
-    with open(os.path.join('sql', 'create_tables.sql')) as f:
-        db.executescript(f.read().decode('utf8'))
+    with open(os.path.join("sql", "create_tables.sql")) as f:
+        db.executescript(f.read().decode("utf8"))
+
 
 def init_app(app: Flask) -> None:
     # app.teardown_appcontext() tells Flask to call that function when cleaning up after returning the response.
     app.teardown_appcontext(close_db)
+
 
 def query_db(query, args=(), one=False):
     """
